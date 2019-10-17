b0ff35c15cacdc9b946e46a3169a6645659b4fdf
hwoongg <gusdnd8536@gmail.com>
2019-10-17 13:19:19 +0900

    윈도우 생성 및 DX 장치 구성.

---
+++ systemclass.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "inputclass.h"
+#include "graphicsclass.h"
+
+// 빌드 속도 상승용
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_
+
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+private:
+	LPCWSTR m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	InputClass* m_Input; // 입력 처리 클래스
+	GraphicsClass* m_Graphics; // 그래픽 출력 클래스
+};
+
+
+/////////////////////////
+// FUNCTION PROTOTYPES //
+/////////////////////////
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+/////////////
+// GLOBALS //
+/////////////
+static SystemClass* ApplicationHandle = 0;
+
+#endif
